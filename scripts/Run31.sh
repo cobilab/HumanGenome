066891c8fb051b6ed5594729fbc1b96b95d1b842
pratas <6882743+pratas@users.noreply.github.com>
2022-06-14 08:53:47 +0100

    run31

---
+++ scripts/Run31.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Install tools:
+conda install -c bioconda geco3 -y
+conda install -c bioconda seqtk -y
+#
+# Download T2T chm13 v2.0 genome:
+rm -f chm13v2.0.fa.gz
+wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz
+#
+# Filter non ACGT data:
+gunzip -k chm13v2.0.fa.gz; 
+seqtk seq -U chm13v2.0.fa | tr -d -c "ACGT" > HS.seq;
+#
+# Data compression:
+time GeCo3 -v -lr 0.005 -hs 100 -tm 1:1:0:0:0.6/0:0:0 -tm 2:1:2:0:0.90/0:0:0 -tm 2:1:1:0:0.8/0:0:0 -tm 3:1:0:0:0.8/0:0:0 -tm 4:1:0:0:0.8/0:0:0 -tm 5:1:0:0:0.8/0:0:0 -tm 6:1:0:0:0.8/0:0:0 -tm 7:1:1:0:0.7/0:0:0 -tm 8:1:0:0:0.85/0:0:0 -tm 9:1:1:0:0.88/0:0:0 -tm 12:20:0:0:0.88/0:0:0 -tm 14:50:1:1:0.89/0:10:0.89 -tm 17:2000:1:10:0.88/2:50:0.88 -tm 20:1200:1:140:0.88/3:10:0.88 HS.seq 1> report_c_stdout.txt 2> report_c_stderr.txt;
+#
+# Data decompression:
+time GeDe3 -v HS.seq.co 1> report_d_stdout.txt 2> report_d_stderr.txt;
+#
+# Lossless validation:
+cmp HS.seq.de HS.seq > cmp.txt;
+#
