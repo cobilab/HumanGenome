9ff67fe25402879ea4a6b3bd3ee5196e72b1c421
pratas <6882743+pratas@users.noreply.github.com>
2022-06-17 11:11:22 +0100

    runs

---
+++ scripts/Run34.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Install tools:
+conda install -c bioconda seqtk -y
+rm -fr v0.2.1.tar.gz bsc-m03-0.2.1
+wget https://github.com/IlyaGrebnov/bsc-m03/archive/refs/tags/v0.2.1.tar.gz
+tar -vxzf v0.2.1.tar.gz
+cd bsc-m03-0.2.1/
+cmake .
+make
+cp bsc-m03 ..
+cd ..
+rm -fr bsc-m03-0.2.1/ v0.2.1.tar.gz
+#
+# Download T2T chm13 v2.0 genome:
+rm -f chm13v2.0.fa.gz
+wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz
+#
+# Filter non ACGT data:
+gunzip -k chm13v2.0.fa.gz; 
+seqtk seq -U chm13v2.0.fa | tr -d -c "ACGT" > HS.seq;
+#
+# Data compression:
+time ./bsc-m03 e HS.seq HS.bsc -b 400000000 1> report_c_stdout.txt 2> report_c_stderr.txt;
+#
+# Data decompression:
+time ./bsc-m03 d HS.bsc HS.seq.de 1> report_d_stdout.txt 2> report_d_stderr.txt;
+#
+# Lossless validation:
+cmp HS.seq.de HS.seq > cmp.txt;
+#
